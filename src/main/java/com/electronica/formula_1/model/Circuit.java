e2a06c945763a9b2bfc5aa7614d63fc2865a191e
Vidhan Gupta <vidhangupta094@gmail.com>
2024-08-12 17:33:09 +0530

    Initial commit for Formula1 Dashboard

---
+++ src/main/java/com/electronica/formula_1/model/Circuit.java
@@ -0,0 +1,9 @@
+package com.electronica.formula_1.model;
+
+import jakarta.persistence.Entity;
+
+//NOTE: This is model for all circuit in formula 1
+@Entity
+public class Circuit{
+
+}
---
+++ src/test/java/com/electronica/formula_1/Formula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronica.formula_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
